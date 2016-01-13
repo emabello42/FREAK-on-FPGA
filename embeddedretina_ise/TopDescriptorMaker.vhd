4883e31f964d10d95223cb493987789f969f61ff
emabello42 <emabello42@gmail.com>
2016-01-12 21:31:26 -0300

    last version

--- embeddedretina_ise/TopDescriptorMaker.vhd
+++ embeddedretina_ise/TopDescriptorMaker.vhd
@@ -1,3 +1,23 @@
+--Copyright 2014 by Emmanuel D. Bello <emabello42@gmail.com>
+--Laboratorio de Computacion Reconfigurable (LCR)
+--Universidad Tecnologica Nacional
+--Facultad Regional Mendoza
+--Argentina
+
+--This file is part of FREAK-on-FPGA.
+
+--FREAK-on-FPGA is free software: you can redistribute it and/or modify
+--it under the terms of the GNU General Public License as published by
+--the Free Software Foundation, either version 3 of the License, or
+--(at your option) any later version.
+
+--FREAK-on-FPGA is distributed in the hope that it will be useful,
+--but WITHOUT ANY WARRANTY; without even the implied warranty of
+--MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--GNU General Public License for more details.
+
+--You should have received a copy of the GNU General Public License
+--along with FREAK-on-FPGA.  If not, see <http://www.gnu.org/licenses/>.
 --------------------------------------------------------------------------------
 -- Copyright (c) 1995-2013 Xilinx, Inc.  All rights reserved.
 --------------------------------------------------------------------------------
